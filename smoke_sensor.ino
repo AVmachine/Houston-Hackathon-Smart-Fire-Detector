749c0fa2d10b1d314d25c54a1f45e6984072a7df
rvmccasland <47196305+rvmccasland@users.noreply.github.com>
2019-05-19 12:33:46 -0500

    Update smoke_sensor.ino

--- smoke_sensor.ino
+++ smoke_sensor.ino
@@ -1,38 +1,35 @@
+//smoke sensor variables
 int redLed = 12;
-int greenLed = 8;
+int greenLed = 11;
 int buzzer = 10;
-int smokeA0 = A5;
-int notes[] = {262, 294, 330, 349};
-int analogPin = A0;
-int analogInput;
+int sensorThres = 570; //threshold value for smoke sensor; can be changed
+//temp sensor variables
 double fTemp;
 double cTemp;
-// Your threshold value
-int sensorThres = 645;
 
 void setup() {
   pinMode(redLed, OUTPUT);
   pinMode(greenLed, OUTPUT);
   pinMode(buzzer, OUTPUT);
-  pinMode(smokeA0, INPUT);
   Serial.begin(9600);
 }
 
-
 void loop() {
-  int analogSensor = analogRead(smokeA0);
-  analogInput = analogRead(A0);
+  int analogSensor = analogRead(A5); //smoke sensor
+  int analogInput = analogRead(A0); //temp sensor
+  
+  //converts analog input to celcius
   cTemp = (double)analogInput / 1024 * 5;
   cTemp = cTemp - 0.5;
   cTemp = cTemp * 100;
-
+  // converts celcius to fahrenheit
   fTemp = (cTemp * 9 / 5) + 32;
-  //Serial.print("TEMP:");
-  //Serial.println(fTemp);
   
-  //Serial.print("Pin A0: ");
-  //Serial.println(analogSensor);
-  // Checks if it has reached the threshold value
+  Serial.print("TEMPERATURE:");
+  Serial.println(fTemp);
+  Serial.print("SMOKE SENSOR: ");
+  Serial.println(analogSensor);
+  
   if (analogSensor > sensorThres && fTemp < 65)
   {
     digitalWrite(redLed, HIGH);
